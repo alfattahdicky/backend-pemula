57fbe9d144bc2def590e3e4f393572ced881d301
alfattahdicky <alfattahdicky2@gmail.com>
2022-02-07 20:52:25 +0700

    Response BOdy

--- web-server/server.js
+++ web-server/server.js
@@ -17,16 +17,22 @@ const requestListener = (request, response) => {
     // TODO 2 : logika respons bila url bernilai '/'
     if(method === 'GET') {
       response.statusCode = 200;
-      response.end('<h1>ini adalah homepage</h1>');
+      response.end(JSON.stringify({
+        message: 'ini adalah homepage',
+      }));
     }else {
       response.statusCode = 400;
-      response.end(`<h1>Halaman tidak dapat diakses dengan ${method} request</h1>`);
+      response.end(JSON.stringify({
+        message: `Halaman tidak dapat diakses dengan ${method} request`,
+      }));
     }
   } else if(url === '/about') {
     // TODO 3 : logika respons bila url bernilai '/about'
     if(method === 'GET') {
       response.statusCode = 200;
-      response.end('<h1>Halo! ini adalah halaman about</h1>');
+      response.end(JSON.stringify({
+        message: 'Halo! ini adalah halaman about',
+      }));
     } else if(method === 'POST') {
       let body = [];
 
@@ -38,17 +44,23 @@ const requestListener = (request, response) => {
         body = Buffer.concat(body).toString();
         const { name } = JSON.parse(body);
         response.statusCode = 200;
-        response.end(`<h1>Halo, ${name} ini adalah halaman about</h1>`);
+        response.end(JSON.stringify({
+          message: `Halo, ${name} ini adalah halaman about</h1>`,
+        }));
       });
     } else {
       response.statusCode = 400;
-      response.end(`<h1>Halaman tidak dapat diakses dengan ${method} request</h1>`);
+      response.end(JSON.stringify({
+        message: `Halaman tidak dapat diakses dengan ${method} request`,
+      }));
     }
 
   }else {
     // TODO 1 : logika respons bila url bukan '/' atau '/about'
     response.statusCode = 404;
-    response.end('<h1>Halaman tidak ditemukan</h1>');
+    response.end(JSON.stringify({
+      message: 'Halaman tidak ditemukan',
+    }));
   }
 
 };
