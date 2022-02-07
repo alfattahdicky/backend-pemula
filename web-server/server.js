39198cfce07e3cac4203e2866751d37a482a13fe
alfattahdicky <alfattahdicky2@gmail.com>
2022-02-07 19:26:39 +0700

    Method Request

---
+++ web-server/server.js
@@ -0,0 +1,41 @@
+const http = require('http');
+
+/**
+ * Logika untuk menangani dan menanggapi request dituliskan pada fungsi ini
+ * 
+ * @param request: objek yang berisikan informasi terkait permintaan
+ * @param response: objek yang digunakan untuk menanggapi permintaan
+ */
+
+const requestListener = (request, response) => {
+  response.setHeader('Content-Type', 'text/html');
+  
+  response.statusCode = 200;
+  
+  const { method } = request;
+  if(method === 'GET') {
+    response.end('<h1>Hallo ini GET</h1>');
+  }
+
+  if(method === 'POST') {
+    response.end('<h1>Hai ini POST</h1>');
+  }
+
+  if(method === 'PUT') {
+    response.end('<h1>Hai ini PUT</h1>');
+  }
+
+  if(method === 'DELETE') {
+    response.end('<h1>Hai ini DELETE</h1>');
+  }
+
+};
+
+const server = http.createServer(requestListener);
+
+const port = 5000;
+const host = 'localhost';
+
+server.listen(port, host, () => {
+  console.log(`Server berjalan pada http://${host}:${port}`);
+});
