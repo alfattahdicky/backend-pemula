8bc12ec59175ed3e32617cf3f31673744b7b4a37
alfattahdicky <alfattahdicky2@gmail.com>
2022-02-08 15:26:22 +0700

    Query Parameters

--- hapi-web-server/routes.js
+++ hapi-web-server/routes.js
@@ -29,9 +29,14 @@ const routes = [
   },
   {
     method: 'GET',
-    path: '/hello/${name?}',
+    path: '/hello/{name?}',
     handler: (request, h) => {
       const { name = "stranger" } = request.params;
+      const { lang } = request.query;
+
+      if(lang === 'id') {
+        return `Hai, ${name}`;
+      }
       return `Hello ${name}!`;
     }
   },
